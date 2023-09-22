a85a7a7ffaf5d2eb857257bf0c4a839fb26a7405
jacie22 <jacie22@gmail.com>
2023-09-22 00:31:31 -0400

    w2

--- scripts/getDate.js
+++ scripts/getDate.js
@@ -1,7 +1,30 @@
 
+//Date
 document.getElementById('yr').innerHTML = new Date().getFullYear();
-
 let text = document.lastModified;
 document.querySelector("#lm").innerHTML = text;
 
 
+
+//Responsive Hamburger Menu
+function toggleMenu() {
+    document.getElementsByClassName('primaryNav')[0].classList.toggle('responsive');
+    
+  }
+
+//Dark Mode
+  const modeButton = document.querySelector("#mode");
+  const main = document.querySelector("main");
+  
+  modeButton.addEventListener("click", () => {
+      if (modeButton.textContent.includes("Dark Mode ⚫")) {
+          main.style.background = "#0f396b";
+          main.style.color = "#faf3dd";
+          modeButton.textContent = "Dark Mode ⚪";
+      } else {
+          main.style.background = "#faf3dd";
+          main.style.color = "#0f396b";
+          modeButton.textContent = "Light Mode ⚫";
+      }
+  });
+  
